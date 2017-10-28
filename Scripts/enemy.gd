9bb0d617309830e7e7f8def8bed2a5f0bbf87c68
Mulham Raee <mulham.raee@outlook.com>
2017-10-28 13:12:05 +0200

    Add shooting logic

---
+++ Scripts/enemy.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _notification(what):
+	if 1337 == what:
+		print("ouch")

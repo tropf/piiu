d77fe41037bb6a2ed7c588aa0d9ad5cf3e38eeac
Rolf Pfeffertal <foo@bar.com>
2017-10-28 17:19:37 +0200

    really ignore ignored worsd

--- GameManager.gd
+++ GameManager.gd
@@ -8,7 +8,7 @@ const PLAYER_1_CONTROLLS = "player1"
 const PLAYER_2_CONTROLLS = "player2"
 
 func _ready():
-	var player_prefab = preload("res://Player.tscn")
+	var player_prefab = preload("res://Prefabs/player.tscn")
 	
 	var player1_pos = get_node("player1_spawn").get_pos()
 	var player2_pos = get_node("player2_spawn").get_pos()

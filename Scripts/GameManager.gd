7360c2b9b11d9c9af852cb1f3d3bc4bfd1099236
Mulham Raee <mulham.raee@outlook.com>
2017-10-28 19:26:14 +0200

    Add menu and level selection

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -6,10 +6,12 @@ extends Node2D
 
 const PLAYER_1_CONTROLLS = "player1"
 const PLAYER_2_CONTROLLS = "player2"
+const player_prefab = preload("res://Prefabs/player.tscn")
 
 func _ready():
-	var player_prefab = preload("res://Prefabs/player.tscn")
-	
+	pass
+
+func spawn_players():
 	var player1_pos = get_node("player1_spawn").get_pos()
 	var player2_pos = get_node("player2_spawn").get_pos()
 	
@@ -23,3 +25,16 @@ func _ready():
 	
 	add_child(player1)
 	add_child(player2)
+
+
+func _on_btn_level1_pressed():
+	var lvl1 = preload("res://Scenes/lvl1.tscn").instance()
+	add_child(lvl1)
+	spawn_players()
+
+func _on_btn_level2_pressed():
+	pass # replace with function body
+
+
+func _on_btn_quit_pressed():
+	get_tree().quit()
---
+++ Scripts/menu.gd
@@ -0,0 +1,16 @@
+extends Container
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _on_btn_newGame_pressed():
+	get_tree().change_scene("res://Scenes/demo_scene.tscn")
+
+func _on_btn_quit_pressed():
+	get_tree().quit()
